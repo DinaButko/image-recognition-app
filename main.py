5fa795e3d645d58a7efeb490533bfddebddc0a79
DzinaButko <butkodina@gmail.com>
2022-07-13 12:21:50 -0400

    added model and pictires

--- main.py
+++ main.py
@@ -10,7 +10,32 @@ app = FastAPI()
 
 @app.get("/", response_class=HTMLResponse)
 async def main():
-    content = """
+    content = head_html + """
     <marquee width="525" behavior="alternate"><h1 style="color:red;font-family:Arial">Please Upload Your Scenes!</h1></marquee>
     """
     return content
+
+
+head_html = """
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+</head>
+<body style="background-color:powderblue;">
+<center>
+"""
+
+
+# Create a function to generate a table
+def get_html_table(image_paths, names, column_labels):
+    s = '<table align="center">'
+    if column_labels:
+        s += '<tr><th><h4 style="font-family:Arial">' + \
+            column_labels[0] + '</h4></th><th><h4 style="font-family:Arial">' + \
+            column_labels[1] + '</h4></th></tr>'
+
+    for name, image_path in zip(names, image_paths):
+        s += '<tr><td><img height="80" src="/' + image_path + '" ></td>'
+        s += '<td style="text-align:center">' + name + '</td></tr>'
+    s += '</table>'
+
+    return s
