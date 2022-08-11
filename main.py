2a9a12fefdf98d559ccadfc806901d1bf16ad65f
mpastore1 <37223493+mpastore1@users.noreply.github.com>
2022-08-10 21:49:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -139,7 +139,7 @@ head_html = """
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
 </head>
-<body style="background-image: url(../DzinaButko/image-recognition-app/AI_Pic.jpg);">
+<body style="background-image: url(https://wallpaper.dog/large/11016940.jpg);">
 <center>
 """
 
